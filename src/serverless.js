7879a0a267fc1a70d487df9d66e4fd26b8e94d65
Thiago Zanivan <hi+github@thiagozf.com>
2020-04-25 17:40:45 -0300

    refactor to run on cloud components

--- src/serverless.js
+++ src/serverless.js
@@ -7,34 +7,26 @@ const {
   deleteCloudFrontDistribution
 } = require('./lib')
 
-/*
- * Website
- */
-
 class CloudFront extends Component {
-  /*
-   * Default
+  /**
+   * Deploy
+   * @param {*} inputs
    */
-
-  async default(inputs = {}) {
-    this.context.status('Deploying')
-
+  async deploy(inputs = {}) {
     inputs.region = inputs.region || 'us-east-1'
     inputs.enabled = inputs.enabled === false ? false : true
     inputs.comment =
       inputs.comment === null || inputs.comment === undefined ? '' : String(inputs.comment)
 
-    this.context.debug(
-      `Starting deployment of CloudFront distribution to the ${inputs.region} region.`
-    )
+    console.log(`Starting deployment of CloudFront distribution to the ${inputs.region} region.`)
 
     const cf = new aws.CloudFront({
-      credentials: this.context.credentials.aws,
+      credentials: this.credentials.aws,
       region: inputs.region
     })
 
     const s3 = new aws.S3({
-      credentials: this.context.credentials.aws,
+      credentials: this.credentials.aws,
       region: inputs.region
     })
 
@@ -42,52 +34,50 @@ class CloudFront extends Component {
       if (
         !equals(this.state.origins, inputs.origins) ||
         !equals(this.state.defaults, inputs.defaults) ||
-        !equals(this.state.enabled, inputs.enabled) ||
-        !equals(this.state.comment, inputs.comment)
+        !equals(this.state.enabled, inputs.enabled)
       ) {
-        this.context.debug(`Updating CloudFront distribution of ID ${this.state.id}.`)
+        console.log(`Updating CloudFront distribution of ID ${this.state.id}.`)
         this.state = await updateCloudFrontDistribution(cf, s3, this.state.id, inputs)
       }
     } else {
-      this.context.debug(`Creating CloudFront distribution in the ${inputs.region} region.`)
+      console.log(`Creating CloudFront distribution in the ${inputs.region} region.`)
       this.state = await createCloudFrontDistribution(cf, s3, inputs)
     }
 
     this.state.region = inputs.region
     this.state.enabled = inputs.enabled
-    this.state.comment = inputs.comment
     this.state.origins = inputs.origins
     this.state.defaults = inputs.defaults
-    await this.save()
 
-    this.context.debug(`CloudFront deployed successfully with URL: ${this.state.url}.`)
+    console.log(`CloudFront deployed successfully with URL: ${this.state.url}.`)
 
-    return this.state
+    return {
+      name: inputs.name,
+      id: this.state.id,
+      arn: this.state.arn,
+      url: this.state.url
+    }
   }
 
   /**
    * Remove
+   * @param {*} inputs
    */
-
-  async remove() {
-    this.context.status(`Removing`)
-
+  async remove(inputs = {}) {
     if (!this.state.id) {
       return
     }
 
     const cf = new aws.CloudFront({
-      credentials: this.context.credentials.aws,
+      credentials: this.credentials.aws,
       region: this.state.region
     })
 
     await deleteCloudFrontDistribution(cf, this.state.id)
 
     this.state = {}
-    await this.save()
 
-    this.context.debug(`CloudFront distribution was successfully removed.`)
-    return {}
+    console.log(`CloudFront distribution was successfully removed.`)
   }
 }
 
